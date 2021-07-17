721f42564eb2db4bc75c1634c936b308e8ba7e19
Gerardo Moreno <wanderup.allego@gmail.com>
2021-07-17 19:45:57 -0400

    doing examples for Form still have a bug that shows a warining, the message on the input fiel does not get erased, and when code is erased it work as intended by the author but I still have the warning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    // console.log('gerardo test', event);
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-            <Note key={note.id} note={note} />
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
