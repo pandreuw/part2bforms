b27c840ec3fece4410f9272baff44ffea6b4eb17
Gerardo Moreno <wanderup.allego@gmail.com>
2021-07-23 14:24:14 -0400

    Doing the exercises for part 2 form, added the button for the important and all

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ const App = (props) => {
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
-    // console.log('gerardo test', event);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
   const handleNoteChange = (event) => {
@@ -18,11 +26,21 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
